aca6976103c99f1675d4e144949d72711fce6393
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-08-23 11:47:34 -0400

    added tab navbar

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,10 +2,27 @@ import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-// TODO: Add missing tabs below
-// Take a look at React Semantic UI tabs 
-// https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
-
+    const panes = [
+        {
+          menuItem: 'Home Page',
+          render: () => <Tab.Pane attached={false}>Home Page</Tab.Pane>,
+        },
+        {
+          menuItem: 'Characters',
+          render: () => <Tab.Pane attached={false}>Characters</Tab.Pane>,
+        },
+        {
+          menuItem: 'Location',
+          render: () => <Tab.Pane attached={false}>Location</Tab.Pane>,
+        },
+        {
+            menuItem: 'Episodes',
+            render: () => <Tab.Pane attached={false}>Episodes</Tab.Pane>,
+        },
+      ]
+    
+      return ( 
+        <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+    )  
 };
-
